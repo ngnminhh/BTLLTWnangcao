2d9235df36d14e0a14f54571912e1152ae3e0c81
Truong <daikooh1234@gmail.com>
2024-11-03 21:52:15 +0700

    first push

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace QLTV.Controllers
+{
+    public class AccountController : Controller
+    {
+        [Route("login")]
+        public IActionResult Index()
+        {
+            ViewData["Title"] = "Đăng nhập";
+            return View("Login");
+        }
+        [Route("signup")]
+        public IActionResult SignUp(){
+            ViewData["Title"] = "Đăng ký";
+            return View("SignUp");
+        }
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QLTV.Models;
+using System.Diagnostics;
+
+namespace QLTV.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [Route("homepage")]
+        public IActionResult Index()
+        {
+            ViewData["Title"] = "Trang chủ";
+            return View();
+        }
+
+        [Route("cart")]
+        public IActionResult Cart()
+        {
+            ViewData["Title"] = "Giỏ hàng";
+            return View();
+        }
+        [Route("itemdetail")]
+        public IActionResult ItemDetail()
+        {
+            ViewData["Title"] = "Chi tiết sản phẩm";
+            return View();
+        }
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
