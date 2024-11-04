0e218b03b9eb3c53dd649e644d860520d93acdc6
Truong <daikooh1234@gmail.com>
2024-11-04 23:20:42 +0700

    Cap nhat giao dien quan tri

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace QLTV.Controllers
+{
+    public class AdminController : Controller
+    {
+        [Route("books_manage")]
+        public IActionResult Index()
+        {
+            ViewData["Title"] = "Quản lý sản phẩm";
+            return View("BooksManage");
+        }
+        [Route("account_manage")]
+        public IActionResult Account()
+        {
+            ViewData["Title"] = "Quản lý người dùng";
+            return View();
+        }
+        [Route("cart_manage")]
+        public IActionResult Cart()
+        {
+            ViewData["Title"] = "Quản lý thẻ mượn";
+            return View();
+        }
+    }
+}
